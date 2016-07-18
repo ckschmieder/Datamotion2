4f196ff1e1a603c78f7167d194a7b45aa10164d8
Chris <ckschmieder@gmail.com>
2016-07-18 16:42:49 -0400

    updated href for IT Pro Solutions button

--- page-home-dev.php
+++ page-home-dev.php
@@ -91,7 +91,7 @@
       <h2 class="wow fadeIn" data-wow-delay=".2s" data-wow-duration="1.4s">Build in security</br>& compliance</h2>
       <p class="wow fadeInDown desk" data-wow-delay=".4s" data-wow-duration="1.2s">Power your communication workflows & apps using our complete set of standard connectors, SDKs & APIs.</p>
       <ul id="lock-menu" class="button-wrap lock desk-menu">
-        <li class="new-button wow flipInX" data-wow-delay=".7s" data-wow-duration="1.5s"><a href="">IT Pro Solutions</a></li>
+        <li class="new-button wow flipInX" data-wow-delay=".7s" data-wow-duration="1.5s"><a href="https://www.datamotion.com/solutions/solving-workflow-compliance-integration-challenges/">IT Pro Solutions</a></li>
         <li class="new-button wow flipInX" data-wow-delay=".9s" data-wow-duration="1.5s"><a href="https://www.datamotion.com/products/securemail/encryption-sdk-datamotion-securemail/">App Developer Solutions</a></li>
       </ul>
     </aside>
@@ -101,7 +101,7 @@
     <p>Power your communication work flows & apps using our complete set of standard connectors, SDKs & APIs.</p>
   </div>
   <ul class="button-wrap lock mob-menu">
-      <li class="new-button"><a href="">IT Pro Solutions</a></li>
+      <li class="new-button"><a href="https://www.datamotion.com/solutions/solving-workflow-compliance-integration-challenges/">IT Pro Solutions</a></li>
       <li class="new-button"><a href="https://www.datamotion.com/products/securemail/encryption-sdk-datamotion-securemail/">App Developer Solutions</a></li>
   </ul>
 
